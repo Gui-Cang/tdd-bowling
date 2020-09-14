61ca7e4af0decea2e00c66d7faf39378a41705ff
Gui-Cang <821192514@qq.com>
2020-09-15 00:14:19 +0800

    [dingke] fix: find logical erro in codes and fix it.

--- src/main/java/bowling/BowlingScore.java
+++ src/main/java/bowling/BowlingScore.java
@@ -4,11 +4,36 @@ import java.util.*;
 
 public class BowlingScore {
 
-    public static int caculateReultScore(int[] scoreOfEachFrame) {
-        int reultScore = 0;
-        for (int i = 0; i < scoreOfEachFrame.length - 2; i++) {
-            reultScore += scoreOfEachFrame[i] + scoreOfEachFrame[i + 1] + scoreOfEachFrame[i + 2];
+    public static int caculateReultScore(int[][] scoreOfEachFrame) {
+        int resultScoreBeforeTenth = 0;
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            if (scoreOfEachFrame[i][0] == 10 && scoreOfEachFrame[i + 1][0] == 10) {
+                resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i + 1][0] + scoreOfEachFrame[i + 2][0];
+            }
+            if (scoreOfEachFrame[i][0] == 10 && scoreOfEachFrame[i + 1][0] < 10) {
+                resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i + 1][0] + scoreOfEachFrame[i + 1][1];
+            }
+            if (scoreOfEachFrame[i][0] < 10 && (scoreOfEachFrame[i][0] + scoreOfEachFrame[i][1] == 10)) {
+                resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i][1] + scoreOfEachFrame[i + 1][0];
+            }
+            if (scoreOfEachFrame[i][0] < 10 && (scoreOfEachFrame[i][0] + scoreOfEachFrame[i][1] < 10)) {
+                resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i][1];
+            }
         }
-        return reultScore;
+        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] == 10) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[11][0];
+        }
+        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] < 10) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[10][1];
+        }
+        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] == 10)) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] + scoreOfEachFrame[10][0];
+        }
+        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] < 10)) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1];
+        }
+        return sum;
     }
+
 }
--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -9,32 +9,63 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BowlingGameTest {
 
-
     @Test
     void should_get_full_score_when_get_all_strike() {
-        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int[][] scoreOfEachFrame = {
+                {10, 0}, {10, 0}, {10, 0},
+                {10, 0}, {10, 0}, {10, 0},
+                {10, 0}, {10, 0}, {10, 0},
+                {10, 0}, {10, 0}, {10, 0}
+        };
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(300, resultSore);
     }
 
     @Test
     void should_get_zero_score_when_lose_all_frame() {
-        int[] scoreOfEachFrame = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[][] scoreOfEachFrame = {
+                {0, 0}, {0, 0}, {0, 0},
+                {0, 0}, {0, 0}, {0, 0},
+                {0, 0}, {0, 0}, {0, 0},
+                {0, 0}
+        };
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(0, resultSore);
     }
 
     @Test
     void should_get_appropriate_sum_score_when_caculate_given_spare() {
-        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 2, 10};
+        int[][] scoreOfEachFrame = {
+                {10, 0}, {10, 0}, {10, 0},
+                {10, 0}, {10, 0}, {10, 0},
+                {10, 0}, {10, 0}, {10, 0},
+                {8, 2}, {10, 0}
+        };
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(278, resultSore);
     }
 
     @Test
-    void should_get_appropriate_sum_score_when_caculate_given_no_strike_nor_spare() {
-        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 1};
+    void should_get_appropriate_sum_score_when_caculate_given_middle_score() {
+        int[][] scoreOfEachFrame = {
+                {9, 0}, {9, 0}, {9, 0},
+                {9, 0}, {9, 0}, {9, 0},
+                {9, 0}, {9, 0}, {9, 0},
+                {9, 0},
+        };
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
-        assertEquals(257, resultSore);
+        assertEquals(90, resultSore);
+    }
+
+    @Test
+    void should_get_appropriate_sum_score_when_caculate_given_middle_score2() {
+        int[][] scoreOfEachFrame = {
+                {9, 1}, {9, 0}, {9, 0},
+                {10, 0}, {9, 0}, {9, 0},
+                {9, 0}, {9, 0}, {9, 0},
+                {9, 0},
+        };
+        int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
+        assertEquals(110, resultSore);
     }
 }

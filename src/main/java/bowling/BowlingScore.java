270a77352b9c36dcd7185965b0c24fb06823d24f
Gui-Cang <821192514@qq.com>
2020-09-15 00:20:54 +0800

    [dingke] refactor: extract method and reformat code.

--- src/main/java/bowling/BowlingScore.java
+++ src/main/java/bowling/BowlingScore.java
@@ -7,6 +7,28 @@ public class BowlingScore {
     public static int caculateReultScore(int[][] scoreOfEachFrame) {
         int resultScoreBeforeTenth = 0;
         int sum = 0;
+        resultScoreBeforeTenth = getResultScoreBeforeTenth(scoreOfEachFrame, resultScoreBeforeTenth);
+        sum = dealWithTenthScore(scoreOfEachFrame, resultScoreBeforeTenth, sum);
+        return sum;
+    }
+
+    private static int dealWithTenthScore(int[][] scoreOfEachFrame, int resultScoreBeforeTenth, int sum) {
+        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] == 10) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[11][0];
+        }
+        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] < 10) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[10][1];
+        }
+        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] == 10)) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] + scoreOfEachFrame[10][0];
+        }
+        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] < 10)) {
+            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1];
+        }
+        return sum;
+    }
+
+    private static int getResultScoreBeforeTenth(int[][] scoreOfEachFrame, int resultScoreBeforeTenth) {
         for (int i = 0; i < 9; i++) {
             if (scoreOfEachFrame[i][0] == 10 && scoreOfEachFrame[i + 1][0] == 10) {
                 resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i + 1][0] + scoreOfEachFrame[i + 2][0];
@@ -21,19 +43,7 @@ public class BowlingScore {
                 resultScoreBeforeTenth += scoreOfEachFrame[i][0] + scoreOfEachFrame[i][1];
             }
         }
-        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] == 10) {
-            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[11][0];
-        }
-        if (scoreOfEachFrame[9][0] == 10 && scoreOfEachFrame[10][0] < 10) {
-            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[10][0] + scoreOfEachFrame[10][1];
-        }
-        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] == 10)) {
-            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] + scoreOfEachFrame[10][0];
-        }
-        if (scoreOfEachFrame[9][0] < 10 && (scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1] < 10)) {
-            sum = resultScoreBeforeTenth + scoreOfEachFrame[9][0] + scoreOfEachFrame[9][1];
-        }
-        return sum;
+        return resultScoreBeforeTenth;
     }
 
 }

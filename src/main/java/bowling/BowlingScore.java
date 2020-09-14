8f13e64831514a0dc7d63f12312ae713b30327db
Gui-Cang <821192514@qq.com>
2020-09-14 20:25:24 +0800

    [dingke] fix: more test for spare occasion and remove treemap

--- src/main/java/bowling/BowlingScore.java
+++ src/main/java/bowling/BowlingScore.java
@@ -1,17 +1,13 @@
 package bowling;
+
 import java.util.*;
 
 public class BowlingScore {
 
-
     public static int caculateReultScore(int[] scoreOfEachFrame) {
         int reultScore = 0;
-        TreeMap<Integer, Integer> treeMapOfEachScore = new TreeMap<Integer, Integer>();
-        for (int i = 0; i < scoreOfEachFrame.length; i++) {
-            treeMapOfEachScore.put(i,scoreOfEachFrame[i]);
-        }
         for (int i = 0; i < scoreOfEachFrame.length - 2; i++) {
-            reultScore += scoreOfEachFrame[i] + scoreOfEachFrame[i+1] + scoreOfEachFrame[i+2];
+            reultScore += scoreOfEachFrame[i] + scoreOfEachFrame[i + 1] + scoreOfEachFrame[i + 2];
         }
         return reultScore;
     }
--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -1,4 +1,5 @@
 package bowling;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,10 +11,33 @@ public class BowlingGameTest {
     @Test
     void should_get_full_score_when_get_all_strike() {
         String strings = "to int";
-        int[] scoreOfEachFrame= {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(300, resultSore);
     }
 
+    @Test
+    void should_get_zero_score_when_lose_all_frame() {
+        String strings = "to int";
+        int[] scoreOfEachFrame = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
+        assertEquals(0, resultSore);
+    }
+
+    @Test
+    void should_get_appropriate_sum_score_when_caculate_given_spare() {
+        String strings = "to int";
+        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 2, 10};
+        int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
+        assertEquals(278, resultSore);
+    }
+
+    @Test
+    void should_get_appropriate_sum_score_when_caculate_given_no_strike_nor_spare() {
+        String strings = "to int";
+        int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 1};
+        int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
+        assertEquals(257, resultSore);
+    }
 
 }

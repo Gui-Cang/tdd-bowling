b00f1f75a1baa1109895d3e5c3ed5f0b4cf2c2ce
Gui-Cang <821192514@qq.com>
2020-09-14 21:01:20 +0800

    [dingke] refactor: delete some useless things in test.

--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BowlingGameTest {
 
+
     @Test
     void should_get_full_score_when_get_all_strike() {
-        String strings = "to int";
         int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(300, resultSore);
@@ -18,7 +19,6 @@ public class BowlingGameTest {
 
     @Test
     void should_get_zero_score_when_lose_all_frame() {
-        String strings = "to int";
         int[] scoreOfEachFrame = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(0, resultSore);
@@ -26,7 +26,6 @@ public class BowlingGameTest {
 
     @Test
     void should_get_appropriate_sum_score_when_caculate_given_spare() {
-        String strings = "to int";
         int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 2, 10};
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(278, resultSore);
@@ -34,10 +33,8 @@ public class BowlingGameTest {
 
     @Test
     void should_get_appropriate_sum_score_when_caculate_given_no_strike_nor_spare() {
-        String strings = "to int";
         int[] scoreOfEachFrame = {10, 10, 10, 10, 10, 10, 10, 10, 10, 8, 1};
         int resultSore = BowlingScore.caculateReultScore(scoreOfEachFrame);
         assertEquals(257, resultSore);
     }
-
 }
